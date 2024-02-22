5bf1ff74c55d5cf4dda8a312f0063f3f3c8826c8
Jiří Zelenka <j3.zelenka@gmail.com>
2024-02-22 22:21:34 +0100

    feat: added custom tkinter window class

---
+++ custom_tkinter_window.py
@@ -0,0 +1,37 @@
+# custom_tkinter_window.py
+import tkinter as tk
+
+class AppWindow(tk.Tk):
+    """ AppWindow is a customizable Tkinter window. """
+    def __init__(self, title: str, width: int, height: int, resizable: bool) -> None:
+        """Initializes the AppWindow class with given params.
+
+        Args:
+            title (str): Sets the window title.
+            width (int): Sets the Tkinter window width.
+            height (int): Sets the Tkinter window height.
+            resizable (bool): Flag to enable or disable resizability.
+        """
+        super().__init__()
+        # Methods to initilize the window
+        self.create_title(title)
+        self.window_settings(width, height, resizable)
+        
+    def window_settings(self, width: int, height: int, resizable: bool) -> None:
+        """Sets the window size and resizability settings.
+
+        Args:
+            width (int): Width of the window.
+            height (int): Height of the window.
+            resizable (bool): True if the window is resizable; else False.
+        """
+        self.geometry(f"{width}x{height}")
+        self.resizable(resizable, resizable)
+        
+    def create_title(self, name: str) -> None:
+        """Sets the window title.
+
+        Args:
+            name (str): The title of the window.
+        """
+        self.title(name)
