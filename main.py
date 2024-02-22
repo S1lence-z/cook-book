6b0b2fed10ee82fde1d039010c69488baf409678
Jiří Zelenka <j3.zelenka@gmail.com>
2024-02-23 00:50:51 +0100

    feat: initialized db in main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 # main.py
 import tkinter as tk
 from custom_window import AppWindow
+from db_login import HOST, USER, PASSWORD, DATABASE
+import mysql.connector
 
 if __name__ == "__main__":
-    app = AppWindow("My App", 1280, 720, False)
-    app.mainloop()
+    # app = AppWindow("My App", 1280, 720, False)
+    # app.mainloop()
+    
+    db_connection = mysql.connector.connect(
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        database=DATABASE
+    )
+    
+    cursor = db_connection.cursor()
+    cursor.execute("select * from recipes")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
