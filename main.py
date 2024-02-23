03442274f210404bcaa4dc0ab1c133efa0df4c6c
S1lence-z <j3.zelenka@gmail.com>
2024-02-24 00:16:53 +0100

    fix: changed imports and window title

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 from db.db_login import HOST, USER, PASSWORD, DATABASE
 import mysql.connector
 from views.custom_window import AppWindow
-from .cook_book_app import CookBookApp
-from .model.recipe_manager import RecipeManager
+from cook_book_app import CookBookApp
+from model.recipe import Recipe
+from model.recipe_manager import RecipeManager
 
 if __name__ == "__main__":
     # Connect to the database
@@ -16,9 +17,9 @@ if __name__ == "__main__":
     # Create the model
     recipe_manager = RecipeManager(db_connection)
     # Create an instance of the app and run it
-    main_window = AppWindow("My App", 1280, 720, False)
+    main_window = AppWindow("CookBook", 1280, 720, False)
     app = CookBookApp(main_window, recipe_manager)
     # Run the tkinter window mainloop
     app.window.mainloop()
     
-    #? Close the database connection? When?
+    #! Close the database connection? When?
