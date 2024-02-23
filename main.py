277fdc7830a3209c43c952447835c081754a611b
Jiří Zelenka <j3.zelenka@gmail.com>
2024-02-23 18:01:27 +0100

    feat: organized files to MVC dev model

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 # main.py
 import tkinter as tk
-from custom_window import AppWindow
-from db_login import HOST, USER, PASSWORD, DATABASE
+from views.custom_window import AppWindow
+from db.db_login import HOST, USER, PASSWORD, DATABASE
 import mysql.connector
 
 if __name__ == "__main__":
-    # app = AppWindow("My App", 1280, 720, False)
-    # app.mainloop()
+    # Create an instance of the app and run it
+    app = AppWindow("My App", 1280, 720, False)
     
+    # Connect to the database
     db_connection = mysql.connector.connect(
         host=HOST,
         user=USER,
         password=PASSWORD,
         database=DATABASE
     )
+    # cursor = db_connection.cursor()
+    # cursor.execute("select * from recipes")
+    # rows = cursor.fetchall()
+    # for row in rows:
+    #     print(row)
     
-    cursor = db_connection.cursor()
-    cursor.execute("select * from recipes")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+    # Run the tkinter window mainloop
+    app.mainloop()
--- views/custom_window.py
+++ views/custom_window.py
@@ -4,7 +4,7 @@ import tkinter as tk
 class AppWindow(tk.Tk):
     """ AppWindow is a customizable Tkinter window. """
     def __init__(self, title: str, width: int, height: int, resizable: bool) -> None:
-        """Initializes the AppWindow class with given params.
+        """ Initializes the AppWindow class with given params.
 
         Args:
             title (str): Sets the window title.
@@ -18,7 +18,7 @@ class AppWindow(tk.Tk):
         self.set_window(width, height, resizable)
         
     def set_window(self, width: int, height: int, resizable: bool) -> None:
-        """Sets the window size and resizability settings.
+        """ Sets the window size and resizability settings.
 
         Args:
             width (int): Width of the window.
@@ -29,7 +29,7 @@ class AppWindow(tk.Tk):
         self.resizable(resizable, resizable)
         
     def create_title(self, name: str) -> None:
-        """Sets the window title.
+        """ Sets the window title.
 
         Args:
             name (str): The title of the window.
