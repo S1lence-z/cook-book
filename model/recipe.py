b3aebbf2a5b61c1b88f0b7a84cbba8ba6a4042fe
Jiří Zelenka <j3.zelenka@gmail.com>
2024-02-23 20:22:15 +0100

    feat: added class to represent a recipe

---
+++ model/recipe.py
@@ -0,0 +1,8 @@
+class Recipe:
+    def __init__(self, id: int, title: str, description: str, prep_time: int, cook_time: int, instructions) -> None:
+        self.id = id
+        self.title = title
+        self.description = description
+        self.prep_time = prep_time
+        self.cook_time = cook_time
+        self.instructions = instructions
