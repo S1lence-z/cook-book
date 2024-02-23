bef541285276953f67505e87b46eb4b93f008c7d
S1lence-z <j3.zelenka@gmail.com>
2024-02-24 00:15:08 +0100

    feat: added dict for queries, fixed list_all_recipes method

--- model/recipe_manager.py
+++ model/recipe_manager.py
@@ -1,17 +1,26 @@
 import mysql.connector
-from recipe import Recipe
+from .recipe import Recipe
 
 class RecipeManager:
     def __init__(self, db_connection):
         self.db_connection = db_connection
         self.cursor = db_connection.cursor()
+        self.queries = {
+            "GetAllRecipes": "SELECT * FROM recipes"
+        }
         
     def list_all_recipes(self) -> list[Recipe]:
         all_recipes = []
-        self.cursor.execute("SELECT * FROM recipes")
+        self.cursor.execute(self.queries["GetAllRecipes"])
         all_recipes_raw = self.cursor.fetchall()
         for recipe in all_recipes_raw:
-            print(recipe)
+            id = recipe[0]
+            title = recipe[1]
+            description = recipe[2]
+            prep_time = recipe[3]
+            cook_time = recipe[4]
+            instructions = recipe[5]
+            all_recipes.append(Recipe(id, title, description, prep_time, cook_time, instructions))
         return all_recipes
             
     def delete_recipe_by_name(self, name: str):
