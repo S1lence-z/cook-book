7c72ad4ddf601c4619f524460e6d0aeb61361f91
S1lence-z <j3.zelenka@gmail.com>
2024-02-24 15:45:54 +0100

    feat: simplified the page abstract class

--- views/page_abstract_class.py
+++ views/page_abstract_class.py
@@ -1,11 +1,7 @@
 # page_abstract_class.py
 from abc import ABC, abstractmethod
-import tkinter as tk
-from .custom_window import AppWindow
 
 class Page(ABC):
-    def create_page_frame(self, window: AppWindow, bg_color: str):
-        # Whole page frame
-        page_frame = tk.Frame(window, bg=bg_color)
-        page_frame.pack(fill=tk.BOTH, expand=True)
-        return page_frame
+    @abstractmethod
+    def create_ui(self) -> None:
+        pass
