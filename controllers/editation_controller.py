64855e98807a812eb2a4fe8bd53e767506ea618f
S1lence-z <j3.zelenka@gmail.com>
2024-02-27 14:00:50 +0100

    feat: added basic functionality to all controllers

--- controllers/editation_controller.py
+++ controllers/editation_controller.py
@@ -1,8 +1,24 @@
 # editation_controller.py
 from models.database_manager import DatabaseManager
 from controllers.view_controller import ViewController
+from controllers.page_controller_abc import PageController
 
-class EditationController:
+class EditationController(PageController):
     def __init__(self, model: DatabaseManager, view: ViewController) -> None:
         self.model = model
         self.view = view
+        self.frame = self.view.pages["editRecipe"]
+        self._add_functionality()
+        
+    def _add_functionality(self):
+        self.frame.save_btn.config(command=self.save_edited_recipe)
+        self.frame.cancel_btn.config(command=self.cancel)
+    
+    def save_edited_recipe(self):
+        #! SAVE THE RECIPE TO THE DB
+        print("RECIPE EDITED")
+        self.view.raise_page("home")
+    
+    def cancel(self):
+        self.view.raise_page("home")
+        return
--- controllers/home_controller.py
+++ controllers/home_controller.py
@@ -2,8 +2,9 @@
 import tkinter as tk
 from models.database_manager import DatabaseManager
 from controllers.view_controller import ViewController
+from controllers.page_controller_abc import PageController
 
-class HomeController:
+class HomeController(PageController):
     def __init__(self, model: DatabaseManager, view: ViewController) -> None:
         self.model = model
         self.view = view
@@ -11,12 +12,15 @@ class HomeController:
         self._add_functionality()
         
     def _add_functionality(self):
-        self.frame.add_button.config(command=self.add_recipe)
+        self.frame.add_btn.config(command=self.add_recipe)
+        self.frame.delete_btn.config(command=self.delete_recipe)
+        self.frame.edit_btn.config(command=self.edit_recipe)
     
     def add_recipe(self) -> None:
-        self.view.raise_page("editRecipe")
-        pass
+        self.view.raise_page("addRecipe")
     
     def delete_recipe(self) -> None:
-        print("delete more")
-        pass
+        print("Recipe Deleted MOREEEEE")
+    
+    def edit_recipe(self):
+        self.view.raise_page("editRecipe")
