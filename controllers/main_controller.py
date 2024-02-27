3b6a98bb21dc2a2998eddda88c559e85a55c6c9a
S1lence-z <j3.zelenka@gmail.com>
2024-02-27 14:01:49 +0100

    feat: added initialization of the addition controller to the main controller

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -3,6 +3,8 @@ from models.database_manager import DatabaseManager
 from controllers.view_controller import ViewController
 from controllers.home_controller import HomeController
 from controllers.editation_controller import EditationController
+from controllers.addition_controller import AdditionController
+from controllers.page_controller_abc import PageController
 
 class MainController:
     def __init__(self, model: DatabaseManager, view: ViewController) -> None:
@@ -10,7 +12,7 @@ class MainController:
         self.view = view
         self.page_controllers = self._init_page_controllers()
     
-    def _init_page_controllers(self) -> list:
+    def _init_page_controllers(self) -> list[PageController]:
         """ Initialize all controllers.
 
         Returns:
@@ -18,7 +20,8 @@ class MainController:
         """
         home_controller = HomeController(self.model, self.view)
         editation_controller = EditationController(self.model, self.view)
-        return [home_controller, editation_controller]
+        addition_controller = AdditionController(self.model, self.view)
+        return [home_controller, editation_controller, addition_controller]
     
     def start_app(self):
         self.view.raise_page("home")
