fc076827a95d77dc4f398323f98e851f4c564bf0
Jiří Zelenka <j3.zelenka@gmail.com>
2024-03-01 19:13:58 +0100

    feat: added get recipe by id method

--- models/database_manager.py
+++ models/database_manager.py
@@ -7,6 +7,7 @@ class DatabaseManager:
         self.cursor = db_connection.cursor()
         self.queries = {
             "GetAllRecipes": "SELECT * FROM recipes;",
+            "GetRecipeById": "SELECT * FROM recipes WHERE recipe_id = %s;",
             "AddRecipe": "INSERT INTO recipes (title, _description, prep_time, cook_time, instructions) VALUES (%s, %s, %s, %s, %s);",
             "DeleteRecipeById": "DELETE FROM recipes WHERE recipe_id = %s;",
             "UpdateRecipe": "UPDATE recipes SET title = %s, _description = %s, prep_time = %s, cook_time = %s, instructions = %s WHERE recipe_id = %s;"
@@ -20,6 +21,12 @@ class DatabaseManager:
             id, title, description, prep_time, cook_time, instructions = recipe
             all_recipes.append(Recipe(id, title, description, prep_time, cook_time, instructions))
         return all_recipes
+    
+    def get_recipe_by_id(self, id: int) -> Recipe:
+        self.cursor.execute(self.queries["GetRecipeById"], (id,))
+        recipe = self.cursor.fetchall()
+        id, title, description, prep_time, cook_time, instructions = recipe[0]
+        return Recipe(id, title, description, prep_time, cook_time, instructions)
             
     def add_recipe(self, title: str, description: str, prep_time: str, cook_time: str, instructions: str):
         recipe_data = (title, description, prep_time, cook_time, instructions)
