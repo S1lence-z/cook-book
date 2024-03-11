d029a6af51ff91604d79a1b0e8dc03da63a4231d
S1lence-z <j3.zelenka@gmail.com>
2024-03-11 01:19:36 +0100

    Improved home page visuals

--- pages/home_page.py
+++ pages/home_page.py
@@ -14,22 +14,31 @@ class HomePage(tk.Frame):
         # Center the content
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
+        # Configure column weights to make buttons split equally
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
 
     def _create_ui(self) -> None:
+        # Header label
         self.header = tkb.Label(self, text="Home Page", font=("Arial", 24), bootstyle=tkb.ACTIVE, anchor="center") # type: ignore
-        self.header.grid(row=0, column=0, columnspan=2, sticky="nsew", padx=10, pady=5)
-
-        self.recipe_list = RecipeList(self, font=("Arial", 26))
-        self.recipe_list.grid(row=1, column=0, columnspan=2, sticky="nsew", padx=10, pady=5)
-
-        self.add_btn = tkb.Button(self, text="Add Recipe", bootstyle=tkb.SUCCESS) # type: ignore
-        self.add_btn.grid(row=2, column=0, sticky="nsew", padx=10, pady=5)
-
-        self.delete_btn = tkb.Button(self, text="Delete Recipe", bootstyle=tkb.DANGER) # type: ignore
-        self.delete_btn.grid(row=3, column=0, sticky="nsew", padx=10, pady=5)
+        self.header.grid(row=0, column=0, columnspan=3, sticky="nsew", padx=10, pady=5)
         
+        # Recipe list
+        self.recipe_list = RecipeList(self, font=("Arial", 26))
+        self.recipe_list.grid(row=1, column=0, columnspan=3, sticky="nsew", padx=10, pady=5)
+        
+        # Add Recipe button
+        self.add_btn = tkb.Button(self, text="Add Recipe", bootstyle=tkb.SUCCESS) # type: ignore
+        self.add_btn.grid(row=2, column=0, sticky="nsew", padx=10, pady=5, ipady=15)
+        
+        # Delete Recipe button
+        self.delete_btn = tkb.Button(self, text="Delete Recipe", bootstyle=tkb.DANGER) # type: ignore
+        self.delete_btn.grid(row=2, column=1, sticky="nsew", padx=10, pady=5, ipady=15)
+        
+        # Edit Recipe button
         self.edit_btn = tkb.Button(self, text="Edit Recipe", bootstyle=tkb.PRIMARY) # type: ignore
-        self.edit_btn.grid(row=4, column=0, sticky="nsew", padx=10, pady=5)
+        self.edit_btn.grid(row=2, column=2, sticky="nsew", padx=10, pady=5, ipady=15)
 
     def update_delete_btn_state(self, event: str) -> None:
         delete_btn = self.delete_btn
