90bdd7b52aaf271810700e45bc4409b652187328
S1lence-z <j3.zelenka@gmail.com>
2024-03-13 13:45:43 +0100

    Closes #7: somewhat designed the add recipe page

--- pages/add_recipe_page.py
+++ pages/add_recipe_page.py
@@ -1,5 +1,6 @@
 # add_recipe_page.py
 import tkinter as tk
+import ttkbootstrap as tkb
 from custom import *
 
 class AddRecipePage(tk.Frame, Page):
@@ -18,43 +19,60 @@ class AddRecipePage(tk.Frame, Page):
             **kwargs: Arbitrary keyword arguments.
         """
         super().__init__(*args, **kwargs)
+        self._format_frame()
         self._create_ui()
+        
+    def _format_frame(self) -> None:
+        # Center the content
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(6, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        # Configure column weights to make buttons split equally
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(3, weight=1)
     
     def _create_ui(self) -> None:
-        """
-        Create the user interface for the Add Recipe Page.
-        """
-        self.header = tk.Label(self, text="Add Recipe Page")
+        self.header = tkb.Label(self, text="Add Recipe Page", font=("Arial", 24), bootstyle=tkb.ACTIVE, anchor="center") # type: ignore
+        self.header.grid(row=0, column=0, columnspan=4, sticky="nsew", padx=10, pady=5)
+
         # Title
-        self.title_label = tk.Label(self, text="Title")
-        self.title_label.pack()
+        self.title_label = tk.Label(self, text="Title", font=("Arial", 12))
+        self.title_label.grid(row=1, column=0, sticky="nsew", padx=10, pady=5)
         self.title_entry = tk.Entry(self)
-        self.title_entry.pack()
+        self.title_entry.grid(row=1, column=1, columnspan=3, sticky="nsew", padx=10, pady=5)
+
         # Description
-        self.description_label = tk.Label(self, text="Description")
-        self.description_label.pack()
+        self.description_label = tk.Label(self, text="Description", font=("Arial", 12))
+        self.description_label.grid(row=2, column=0, sticky="nsew", padx=10, pady=5)
         self.description_text = tk.Text(self, height=10)
-        self.description_text.pack()
+        self.description_text.grid(row=2, column=1, columnspan=3, sticky="nsew", padx=10, pady=5)
+
         # Prep Time
-        self.prep_time_label = tk.Label(self, text="Prep Time (minutes)")
-        self.prep_time_label.pack()
+        self.prep_time_label = tk.Label(self, text="Prep Time (minutes)", font=("Arial", 12))
+        self.prep_time_label.grid(row=3, column=0, sticky="nsew", padx=10, pady=5)
         self.prep_time_entry = tk.Entry(self)
-        self.prep_time_entry.pack()
+        self.prep_time_entry.grid(row=3, column=1, sticky="nsew", padx=10, pady=5)
+
         # Cook Time
-        self.cook_time_label = tk.Label(self, text="Cook Time (minutes)")
-        self.cook_time_label.pack()
+        self.cook_time_label = tk.Label(self, text="Cook Time (minutes)", font=("Arial", 12))
+        self.cook_time_label.grid(row=3, column=2, sticky="nsew", padx=10, pady=5)
         self.cook_time_entry = tk.Entry(self)
-        self.cook_time_entry.pack()
+        self.cook_time_entry.grid(row=3, column=3, sticky="nsew", padx=10, pady=5)
+
         # Instructions
-        self.instructions_label = tk.Label(self, text="Instructions")
-        self.instructions_label.pack()
+        self.instructions_label = tk.Label(self, text="Instructions", font=("Arial", 12))
+        self.instructions_label.grid(row=5, column=0, sticky="nsew", padx=10, pady=5)
         self.instructions_text = tk.Text(self, height=10)
-        self.instructions_text.pack()
-        # Buttons
-        self.save_btn = tk.Button(self, text="Save")
-        self.save_btn.pack()
-        self.cancel_btn = tk.Button(self, text="Cancel")
-        self.cancel_btn.pack()
+        self.instructions_text.grid(row=5, column=1, columnspan=3, sticky="nsew", padx=10, pady=5)
+
+        # Add button
+        self.save_btn = tkb.Button(self, text="Add", bootstyle=tkb.SUCCESS) # type: ignore
+        self.save_btn.grid(row=6, column=1, columnspan=2, sticky="nsew", padx=10, pady=5, ipady=15)
+        # Cancel button
+        self.cancel_btn = tkb.Button(self, text="Cancel", bootstyle=tkb.DANGER) # type: ignore
+        self.cancel_btn.grid(row=6, column=3, sticky="nsew", padx=10, pady=5, ipady=15)
         
     def _clear_add_page(self):
         """
