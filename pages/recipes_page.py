a508ad661c64cfb5c219584bb2df7a91224f0722
Jiří Zelenka <j3.zelenka@gmail.com>
2024-03-18 23:52:45 +0100

    Converted recipes page and recipes controller to use customtkinter

--- pages/recipes_page.py
+++ pages/recipes_page.py
@@ -1,8 +1,7 @@
-import tkinter as tk
-import ttkbootstrap as tkb
+import customtkinter as tkc
 from custom import *
 
-class RecipesPage(tk.Frame, Page):
+class RecipesPage(tkc.CTkFrame, Page):
     """A class representing the Recipes Page."""
 
     def __init__(self, *args, **kwargs) -> None:
@@ -40,11 +39,11 @@ class RecipesPage(tk.Frame, Page):
         font_settings = (font_family, font_size)
 
         # Header label
-        self.header = tkb.Label(self, text="Recipes Page", font=(font_family, 24), bootstyle=tkb.ACTIVE, anchor="center") # type: ignore
+        self.header = tkc.CTkLabel(self, text="Recipes Page", font=(font_family, 24)) # type: ignore
         self.header.grid(row=0, column=0, columnspan=3, sticky="nsew", padx=10, pady=5)
 
         # Search bar
-        self.search_bar_label = tkb.Label(self, text="Search by title:", font=font_settings, bootstyle=tkb.ACTIVE) # type: ignore
+        self.search_bar_label = tkc.CTkLabel(self, text="Search by title:", font=font_settings) # type: ignore
         self.search_bar_label.grid(row=1, column=0, padx=10, pady=5)
         self.search_bar = SearchBar(self, font=font_settings)
         self.search_bar.grid(row=1, column=1, columnspan=2, sticky="nsew", padx=10, pady=5)
@@ -54,19 +53,19 @@ class RecipesPage(tk.Frame, Page):
         self.recipe_list.grid(row=2, column=0, columnspan=3, sticky="nsew", padx=10, pady=5)
 
         # Add Recipe button
-        self.add_btn = tkb.Button(self, text="Add Recipe", bootstyle=tkb.SUCCESS) # type: ignore
+        self.add_btn = tkc.CTkButton(self, text="Add Recipe")
         self.add_btn.grid(row=3, column=0, sticky="nsew", padx=10, pady=5, ipady=15)
 
         # Delete Recipe button
-        self.delete_btn = tkb.Button(self, text="Delete Recipe", bootstyle=tkb.DANGER) # type: ignore
+        self.delete_btn = tkc.CTkButton(self, text="Delete Recipe")
         self.delete_btn.grid(row=3, column=1, sticky="nsew", padx=10, pady=5, ipady=15)
 
         # Edit Recipe button
-        self.edit_btn = tkb.Button(self, text="Edit Recipe", bootstyle=tkb.PRIMARY) # type: ignore
+        self.edit_btn = tkc.CTkButton(self, text="Edit Recipe")
         self.edit_btn.grid(row=3, column=2, sticky="nsew", padx=10, pady=5, ipady=15)
 
         # Show Recipe button
-        self.detail_btn = tkb.Button(self, text="Show Recipe", bootstyle=tkb.INFO) #type: ignore
+        self.detail_btn = tkc.CTkButton(self, text="Show Recipe")
         self.detail_btn.grid(row=4, column=0, columnspan=3, sticky="nsew", padx=10, pady=5, ipady=15)
 
     def update_buttons_visibility(self, event: str) -> None:
@@ -85,10 +84,10 @@ class RecipesPage(tk.Frame, Page):
         # Enable selected buttons if a recipe is selected, else disable it
         for button in buttons_to_update:
             try:
-                button.config(state=tk.NORMAL) if recipe_list.curselection() else button.config(state=tk.DISABLED)
+                button.configure(state=tkc.NORMAL) if recipe_list.curselection() else button.configure(state=tkc.DISABLED)
             except IndexError:
-                button.config(state=tk.DISABLED)
-                recipe_list.selection_clear(0, tk.END)
+                button.configure(state=tkc.DISABLED)
+                recipe_list.selection_clear(0, tkc.END)
 
     def _update_recipe_list(self, new_list: list[Recipe]) -> None:
         """
@@ -105,7 +104,7 @@ class RecipesPage(tk.Frame, Page):
         """
         Clear the search bar.
         """
-        self.search_bar.delete(0, tk.END)
+        self.search_bar.delete(0, tkc.END)
 
     def refresh_page(self, all_recipes: list[Recipe]) -> None:
         """
