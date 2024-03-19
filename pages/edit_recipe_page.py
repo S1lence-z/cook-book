a6fab012db270f01c51fcd2f7a96b59d0d1853a1
Jiří Zelenka <j3.zelenka@gmail.com>
2024-03-19 10:22:38 +0100

    Converted edit recipe page to custom tkinter

--- pages/edit_recipe_page.py
+++ pages/edit_recipe_page.py
@@ -1,10 +1,7 @@
-from calendar import c
-import tkinter as tk
-from unicodedata import category
-import ttkbootstrap as tkb
+import customtkinter as tkc
 from custom import *
 
-class EditRecipePage(tk.Frame, Page):
+class EditRecipePage(tkc.CTkFrame, Page):
     """A class representing the Edit Recipe Page.
 
     This page allows the user to edit a recipe's details such as title, description, prep time, cook time, and instructions.
@@ -34,50 +31,50 @@ class EditRecipePage(tk.Frame, Page):
 
     def _create_ui(self) -> None:
         """Creates the user interface elements for the Edit Recipe Page."""
-        self.header = tkb.Label(self, text="Edit Recipe Page", font=("Arial", 24), bootstyle=tkb.ACTIVE, anchor="center") # type: ignore
+        self.header = tkc.CTkLabel(self, text="Edit Recipe Page", font=("Arial", 24), anchor="center")
         self.header.grid(row=0, column=0, columnspan=4, sticky="nsew", padx=10, pady=5)
 
         # Title
-        self.title_label = tk.Label(self, text="Title", font=("Arial", 12))
+        self.title_label = tkc.CTkLabel(self, text="Title", font=("Arial", 12))
         self.title_label.grid(row=1, column=0, sticky="nsew", padx=10, pady=5)
-        self.title_entry = tk.Entry(self)
+        self.title_entry = tkc.CTkEntry(self)
         self.title_entry.grid(row=1, column=1, columnspan=3, sticky="nsew", padx=10, pady=5)
 
         # Description
-        self.description_label = tk.Label(self, text="Description", font=("Arial", 12))
+        self.description_label = tkc.CTkLabel(self, text="Description", font=("Arial", 12))
         self.description_label.grid(row=2, column=0, sticky="nsew", padx=10, pady=5)
-        self.description_text = tk.Text(self, height=10)
+        self.description_text = tkc.CTkTextbox(self, height=10)
         self.description_text.grid(row=2, column=1, columnspan=3, sticky="nsew", padx=10, pady=5)
 
         # Prep Time
-        self.prep_time_label = tk.Label(self, text="Prep Time (minutes)", font=("Arial", 12))
+        self.prep_time_label = tkc.CTkLabel(self, text="Prep Time (minutes)", font=("Arial", 12))
         self.prep_time_label.grid(row=3, column=0, sticky="nsew", padx=10, pady=5)
-        self.prep_time_entry = tk.Entry(self)
+        self.prep_time_entry = tkc.CTkEntry(self)
         self.prep_time_entry.grid(row=3, column=1, sticky="nsew", padx=10, pady=5)
 
         # Cook Time
-        self.cook_time_label = tk.Label(self, text="Cook Time (minutes)", font=("Arial", 12))
+        self.cook_time_label = tkc.CTkLabel(self, text="Cook Time (minutes)", font=("Arial", 12))
         self.cook_time_label.grid(row=3, column=2, sticky="nsew", padx=10, pady=5)
-        self.cook_time_entry = tk.Entry(self)
+        self.cook_time_entry = tkc.CTkEntry(self)
         self.cook_time_entry.grid(row=3, column=3, sticky="nsew", padx=10, pady=5)
 
         # Instructions
-        self.instructions_label = tk.Label(self, text="Instructions", font=("Arial", 12))
+        self.instructions_label = tkc.CTkLabel(self, text="Instructions", font=("Arial", 12))
         self.instructions_label.grid(row=5, column=0, sticky="nsew", padx=10, pady=5)
-        self.instructions_text = tk.Text(self, height=10)
+        self.instructions_text = tkc.CTkTextbox(self, height=10)
         self.instructions_text.grid(row=5, column=1, columnspan=3, sticky="nsew", padx=10, pady=5)
         
         # Category
-        self.category_label = tk.Label(self, text="Category", font=("Arial", 12))
+        self.category_label = tkc.CTkLabel(self, text="Category", font=("Arial", 12))
         self.category_label.grid(row=4, column=0, sticky="nsew", padx=10, pady=5)
-        self.category_entry = tk.Entry(self)
+        self.category_entry = tkc.CTkEntry(self)
         self.category_entry.grid(row=4, column=1, columnspan=3, sticky="nsew", padx=10, pady=5)
 
         # Save button
-        self.save_btn = tkb.Button(self, text="Save", bootstyle=tkb.SUCCESS) # type: ignore
+        self.save_btn = tkc.CTkButton(self, text="Save")
         self.save_btn.grid(row=6, column=1, columnspan=2, sticky="nsew", padx=10, pady=5, ipady=15)
         # Cancel button
-        self.cancel_btn = tkb.Button(self, text="Cancel", bootstyle=tkb.DANGER) # type: ignore
+        self.cancel_btn = tkc.CTkButton(self, text="Cancel")
         self.cancel_btn.grid(row=6, column=3, sticky="nsew", padx=10, pady=5, ipady=15)
 
     def set_recipe_to_edit(self, recipe: Recipe):
@@ -92,27 +89,27 @@ class EditRecipePage(tk.Frame, Page):
     def _clear_edit_page(self):
         """Clears the edit page by resetting the input fields."""
         # Clear the title entry
-        self.title_entry.delete(0, tk.END)
+        self.title_entry.delete(0, tkc.END)
         # Clear the description text.
-        self.description_text.delete('1.0', tk.END)
+        self.description_text.delete('1.0', tkc.END)
         # Clear the prep time entry
-        self.prep_time_entry.delete(0, tk.END)
+        self.prep_time_entry.delete(0, tkc.END)
         # Clear the cook time entry
-        self.cook_time_entry.delete(0, tk.END)
+        self.cook_time_entry.delete(0, tkc.END)
         # Clear the instructions text.
-        self.instructions_text.delete('1.0', tk.END)
+        self.instructions_text.delete('1.0', tkc.END)
         # Clear the category entry
-        self.category_entry.delete(0, tk.END)
+        self.category_entry.delete(0, tkc.END)
             
     def _fill_the_edit_page(self) -> None:
         """Fills the edit page with the recipe's current values."""
         self._clear_edit_page()
         # Fill the form with the recipe's current values
         self.title_entry.insert(0, self._recipe_to_edit.title)
-        self.description_text.insert(tk.END, self._recipe_to_edit.description)
+        self.description_text.insert(tkc.END, self._recipe_to_edit.description)
         self.prep_time_entry.insert(0, str(self._recipe_to_edit.prep_time))
         self.cook_time_entry.insert(0, str(self._recipe_to_edit.cook_time))
-        self.instructions_text.insert(tk.END, self._recipe_to_edit.instructions)
+        self.instructions_text.insert(tkc.END, self._recipe_to_edit.instructions)
         self.category_entry.insert(0, self._recipe_to_edit.category.value)
         
     def get_edited_recipe(self):
