f3a44dda2bf6b813c34d0ec243c0e2045a80f0cf
Jiří Zelenka <j3.zelenka@gmail.com>
2024-03-19 19:45:12 +0100

    Modified search bar to use customtkinter

--- custom/search_bar.py
+++ custom/search_bar.py
@@ -1,6 +1,6 @@
-import tkinter as tk
+import customtkinter as tkc
 
-class SearchBar(tk.Entry):
+class SearchBar(tkc.CTkEntry):
     """A custom search bar widget."""
 
     def __init__(self, *args, **kwargs) -> None:
