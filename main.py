f43722d2775c0a0dc9710a5dd2fb8557519ac0c0
S1lence-z <j3.zelenka@gmail.com>
2024-03-20 20:43:39 +0100

    Updated docstrings for main

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from models.database_manager import DatabaseManager
 import mysql.connector
 
 if __name__ == "__main__":
+    """Main entry point of the program."""
+    
     # Connect to the database
     db_connection = mysql.connector.connect(
         host = HOST,  # Database host
@@ -11,12 +13,16 @@ if __name__ == "__main__":
         password = PASSWORD,  # Database password
         database = DATABASE  # Database name
     )
-    # Create an instance of the tkinter app window and run it and create the model
+    
+    # Create an instance of the database manager
     recipe_manager = DatabaseManager(db_connection)
+    
     # Create the CookBookApp
     app = CookBookApp(recipe_manager)
+    
     # Start the app
     app.start_app()
+    
     # Close the database connection after the app is closed
     db_connection.close()
     print("Database connection closed.")
